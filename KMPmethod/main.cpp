2ccc7d59ecbd3915ff012bf98aee2d51a5b074aa
tseyu26 <newbeeu2022@outlook.com>
2023-01-11 13:39:52 +0800

    add search

--- KMPmethod/main.cpp
+++ KMPmethod/main.cpp
@@ -26,8 +26,8 @@ void bruteForce(char *S, char *P){
 }
 
 // KMP method
-
-void get_next(const char* p, int * next){
+// getNext on textbook, hard to understand
+void getNext(const char* p, int * next){
     int k = -1, j=0;
     next[0] = -1;
     while ( j < strlen(p)-1){
@@ -40,14 +40,72 @@ void get_next(const char* p, int * next){
     }
 }
 
-int main(){
-    char S[] = "aaaaaaabc";
-    char P[] = "abaabcaba";
-    bruteForce(S,P);
-    int *pmt = new int[strlen(P)];
-    get_next(P,pmt);
-    for (int i{0};i<strlen(P);i++){
-        std::cout << pmt[i] << " ";
+//
+void prefixTable(const char* p, int *pmt, int n){ // p means the module string, n means the length of pmt
+    pmt[0] = 0;
+    int len = 0;     // in string
+    int i = 1;       // in pmt
+    while ( i < n){
+        if ( p[i] == p[len]){
+            len++;
+            pmt[i] = len;
+            i++;
+        }else{
+            if (len >  0){
+                len = pmt[len-1];
+            }else{
+                pmt[i] = len;
+                i++;
+            }
+        }
     }
+}
+
+void movePmt (int *pmt, int n){
+    int i;
+    for (i=n-1;i>0;i--){
+        pmt[i] = pmt[i-1];
+    }
+    pmt[0] =-1;
+}
+
+void kmpSearch(const char *text, const char *pattern){
+    int n = strlen(pattern);
+    int *pmt = new int[n];
+    prefixTable(pattern,pmt,n);
+   
+    movePmt(pmt, n);
+
+    // text[i]     len:m
+    // pattern[j]  len:n
+    int i{0};
+    int j{0};
+    int m = strlen(text);
+
+    while (i < m){
+        if (j == n-1 && pattern[j]==text[i]){
+            std::cout << "Found @ "<< i-j << "\n";
+            j = pmt[j];
+        }
+        if (text[i] == pattern[j]){
+            i++; j++;
+        }else{
+            j = pmt[j];
+            if (j == -1){
+                i++;
+                j++;
+            }
+        }
+    }
+
+}
+
+int main(){
+    char pattern[] = "ababcabaa";
+    char text[] = "abababcabaabababab";
+    //bruteForce(text,pattern);
+
+    kmpSearch( text, pattern);
+
     return 0;
 }
